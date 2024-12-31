33e7805f59e06ec19c5b1600efbdacd08685f307
GOROman <yoshihito.kondoh@gmail.com>
2024-12-31 18:40:16 +0900

    WiFi 接続サンプルのコンポーネントを追加

--- main/main.c
+++ main/main.c
@@ -5,7 +5,7 @@
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "esp_wifi.h"
-
+#include "protocol_examples_common.h"
 
 void app_main(void) {
 
@@ -18,6 +18,7 @@ void app_main(void) {
     // Wi-Fi を開始
     esp_event_loop_create_default();
 
+    example_connect();
 
     printf("Hello world!\n");
 

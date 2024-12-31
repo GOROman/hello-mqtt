81c1423e701bb2f02012da6229d6a403ca77bd37
GOROman <yoshihito.kondoh@gmail.com>
2024-12-31 19:15:07 +0900

    MQTTの処理を追加。READMEに説明を記載。

--- README.md
+++ README.md
@@ -1,10 +1,58 @@
 # hello-mqtt
 
-ESP-IDF で MQTT にチャレンジしよう
+ESP-IDF で MQTT にチャレンジしよう！
 
 ## 概要
 
 IoT関連でよく聞く MQTT(Message Queuing Telemetry Transport)、名前は知っていてもイマイチよくわからん。 我々捜査班はMQTTの謎に迫るべくリポジトリを作成した...。
 
 ## MQTT とは？
+
+センサー（温度計とか）のデータをネット経由で送ったり、ボタンの動きを同期したりなどできる。複数のデバイスを同時に動かすなどもカンタンにできる。
+
+## MQTT の仕組み
+
+メッセージを発信できる(Publish)。購読してるマン(Subscribe)に全部届くような仕組み。届けるために中継のサーバーが必要。そのサーバーの事をブローカーと呼ぶ。
+
+## ブローカー(サーバー)をローカルで動かす方法
+
+クラウドサービス(Adafruit IO)などを使うとカンタン。ローカルで試したい場合はMacだとemqxがカンタン。
+
+インストール方法
+~~~
+brew install emqx
+~~~
+
+起動方法
+~~~
+/opt/homebrew/opt/emqx/bin/emqx foreground
+~~~
+
+
+## クライアント
+
+Macだとこの辺のGUIがある。
+
+- https://github.com/emqx/MQTTX
+
+## ESP-IDFでの使用法
+
+- 設定
+~~~
+idf.py menuconfig
+~~~
+
+menuconfig で WifiのSSID と PASSWORD を設定
+
+ソースコード内の```BROKER_URI``` と ```FEED``` を適当に書き換えてください。
+
+- ビルド
+~~~
+idf.py build
+~~~
+
+- 実行
+~~~
+idf.py flash monitor
+~~~
 
--- main/main.c
+++ main/main.c
@@ -7,20 +7,89 @@
 #include "esp_wifi.h"
 #include "protocol_examples_common.h"
 
+#include "mqtt_client.h"
+
+#define BROKER_URI "mqtt://192.168.0.191"
+#define FEED "/topic/led0"
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+
+    esp_mqtt_event_handle_t e = event_data;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
+        case MQTT_EVENT_CONNECTED:
+            printf("[MQTT_EVENT_CONNECTED]\n");
+
+            // Publish
+            esp_mqtt_client_publish(e->client, FEED, "from ESP32!", 0, 0, 0);
+
+            // Subscribe
+            esp_mqtt_client_subscribe(e->client, FEED, 0);
+
+            break;
+
+        case MQTT_EVENT_DISCONNECTED:
+            printf("[MQTT_EVENT_DISCONNECTED]\n");
+            break;
+
+        case MQTT_EVENT_SUBSCRIBED:
+            printf("[MQTT_EVENT_SUBSCRIBED]\n");
+            break;
+
+        case MQTT_EVENT_UNSUBSCRIBED:
+            printf("[MQTT_EVENT_UNSUBSCRIBED]\n");
+            break;
+
+        case MQTT_EVENT_DATA:
+            printf("[MQTT_EVENT_DATA]\n");
+            printf("TOPIC=%.*s\r\n", e->topic_len, e->topic);
+            printf("DATA=%.*s\r\n", e->data_len, e->data);
+            break;
+
+        default:
+        printf("MQTT event %ld\n", event_id);
+            break;
+    }
+}
+
+// MQTT開始
+void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URI,
+    };
+
+    printf("MQTT client initialized\n");
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+
+    printf("MQTT client register\n");
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+
+    printf("MQTT client start\n");
+    esp_mqtt_client_start(client);
+}
+
 void app_main(void) {
 
     // NVS をぶち消す
     nvs_flash_init();
+    printf("NVS flash initialized\n");
 
     // Network Interface を初期化
     esp_netif_init();
+    printf("Network Interface initialized\n");
 
-    // Wi-Fi を開始
+    // Event Loop を作成
     esp_event_loop_create_default();
+    printf("Event loop created\n");
 
+    // Wi-Fi に接続（サンプル）
     example_connect();
+    printf("Connected to Wi-Fi\n");
 
-    printf("Hello world!\n");
+    // MQTT開始
+    mqtt_app_start();
 
     while (1) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);

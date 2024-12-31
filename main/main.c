2d0d0b2f4b6d929e8577a248fb5e6f9219824607
GOROman <yoshihito.kondoh@gmail.com>
2024-12-31 18:29:41 +0900

    NVSの初期化

--- main/main.c
+++ main/main.c
@@ -3,9 +3,13 @@
 #include "freertos/task.h"
 
 #include "esp_system.h"
-#include "esp_log.h"
+#include "nvs_flash.h"
+
 
 void app_main(void) {
+
+    // NVS をぶち消す
+    nvs_flash_init();
 
     printf("Hello world!\n");
 
